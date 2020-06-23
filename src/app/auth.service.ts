a44caa32f1e3e64d273901543f49fe60be80b1bc
Vladyslav Havryliak <vladyslav_havryliak@epam.com>
2020-06-23 16:10:26 +0300

    final

---
+++ src/app/auth.service.ts
@@ -0,0 +1,19 @@
+export class AuthService {
+  loggedIn = false;
+
+  isAuth() {
+    const promise = new Promise((res, rej) => {
+      setTimeout(() => {
+        res(this.loggedIn);
+      }, 800);
+    });
+    return promise;
+  }
+
+  login() {
+    this.loggedIn = true;
+  }
+  logout() {
+    this.loggedIn = false;
+  }
+}
